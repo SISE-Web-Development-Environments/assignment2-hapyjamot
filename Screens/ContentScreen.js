b45066f01f7d66151fe7635014252e60ff45cfb1
HReyes <reyes@post.bgu.ac.il>
2020-04-29 19:42:29 +0300

    pages merging no. 1

--- Screens/ContentScreen.js
+++ Screens/ContentScreen.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-    appendElementByID("welcome");
+    //set menu
+    $("#menu_welcome").click(function(){showInContentByID("welcome")});
+    $("#menu_register").click(function(){showInContentByID("registration")});
+    $("#menu_login").click(function(){showInContentByID("login")});
+    //set content
+    $('.page').hide();
+    $('#welcome').show("fast","swing");
+    //set welcome page
+    $("#welcome_to_login").click(function(){showInContentByID("login")});
+    $("#welcome_to_registration").click(function(){showInContentByID("registration")});
+    //set login page
+    $("#login_to_register").click(function(){showInContentByID("registration")});
+    $("#login_to_welcome").click(function(){showInContentByID("welcome")});
+    //set register page
+    $("#register_to_login").click(function(){showInContentByID("login")});
+    $("#register_to_welcome").click(function(){showInContentByID("welcome")});
 })
-function appendElementByID(id){
-    var element = document.getElementById(id);
-    $('#content_container').append(element);
+function showInContentByID(id){
+    $('.page').hide();
+    $('#'.concat(id)).show("fast","swing");
 }
--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,45 +1,35 @@
-function setUpLoginScreen(){
-    //set up the input elements
-    var userNameText = document.createElement("input");
-    var UserPassword = document.createElement("input");
-    var loginButton = document.createElement("button");
-    var body = document.getElementsByTagName("body");
-    body.console.write("<input type=\"text\" name=\"userName\"><br>");
-    body.console.write("<input type=\"password\" name=\"userPassword\"><br>");
-    body.console.write("<input type=\"submit\" name=\"LoginButton\"><br>");
-    //body.chilred.add(userNameText);
-    //body.chilred.add(UserPassword);
-    //body.chilred.add(loginButton);
-}
 localStorage.setItem("p","p");
-            $(document).ready(function(){
-                $("#login_button_id").click(function(){
-                    $(".login_element").toggle("slow","swing");
-                });
-                $("#send_button").click(function(){
-                    authenticate(
-                    document.getElementById("username_id_test").value,
-                    document.getElementById("password_id_test").value);
-                    });
-
-            });
-            function authenticate(userName,password){
-                var UserLabel = document.getElementById("after_login_label");
-                var GuestLabal = document.getElementById("guest_label");   
-                var LoginButton = document.getElementById("login_button_id");              
-                if(localStorage.getItem(userName)!=null){
-                    UserLabel.innerText="Welcome, ";
-                    UserLabel.append(userName);
-                    UserLabel.hidden=false;
-                    GuestLabal.hidden=true;
-                    $(".login_element").toggle("fast","swing");
-                    LoginButton.innerText="Switch Account";
-                }
-                else{
-                    UserLabel.hidden=true;
-                    UserLabel.innerText="Welcome, ";
-                    GuestLabal.hidden=false;
-                    LoginButton.innerText="Login";
-                    $(".login_element").toggle("fast","swing");
-                }
-            };
+$(document).ready(function(){
+    $("#login_button_id").click(function(){
+         $(".login_element").toggle("slow","swing");
+    });
+    $("#send_button").click(function(){
+         authenticate(
+        document.getElementById("login_username_id").value,
+        document.getElementById("login_password_id").value);
+    });
+});
+function authenticate(userName,password){
+    var UserLabel = document.getElementById("after_login_label");  
+    var LoginButton = document.getElementById("send_button");
+    var details = localStorage.getItem(userName);           
+    if(details!=null){
+        var insertedPassword = details.split("\"")[3];
+        if(insertedPassword==password){
+            UserLabel.innerText="Welcome, ";
+            UserLabel.append(userName);
+            UserLabel.hidden=false;
+            document.getElementById("login_username_id").value="";
+            document.getElementById("login_password_id").value="";
+            LoginButton.innerText="Switch Account";
+        }
+        else{
+            alert("Wrong user details, please try again.")
+        }
+    }
+    else{
+        UserLabel.innerText="Welcome, ";
+        LoginButton.innerText="Login";
+        alert("Wrong user details, please try again.")
+    }
+};
