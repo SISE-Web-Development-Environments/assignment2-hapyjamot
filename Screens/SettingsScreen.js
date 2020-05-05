93d8346abdb2e165cceb1bc91895f7ab4e9d04d0
Sapir-Shamay <shamay.sapir@gmail.com>
2020-05-05 21:17:52 +0300

    fix: bug in modal settings

--- Screens/SettingsScreen.js
+++ Screens/SettingsScreen.js
@@ -1,48 +1,78 @@
 var chosen = null;
+var noConfirm = false;
 var chosenKeys = {
-    keyUp: 'ArrowUp'
+    keyUp: 'ArrowUp',
+    keyDown: 'ArrowDown',
+    keyLeft: 'ArrowLeft',
+    keyRight: 'ArrowRight',
 }
 $(document).ready(function() {
     $("#save_settings").click(function(){
         settingToggle("show");
         showInContentByID("app");
     });
+    $("#chosenUpConfirmed").text(chosenKeys.keyUp)
+    $("#chosenDownConfirmed").text(chosenKeys.keyDown)
+    $("#chosenLeftConfirmed").text(chosenKeys.keyLeft)
+    $("#chosenRightConfirmed").text(chosenKeys.keyRight)
 
-    handleKeyEvent("linkUp","keyUp","chosenUp");
+    handleKeyEvent("linkUp","keyUp","chosenUp","confirmKeyUp");
     handleKeyEvent("linkDown","keyDown","chosenDown");
     handleKeyEvent("linkLeft","keyLeft","chosenLeft");
     handleKeyEvent("linkRight","keyRight","chosenRight");
 })
-// function validateKey
 
-// function save
-
-
-function handleKeyEvent(button, modal, saveField){
-    $("#".concat(button)).click(function() {
-        $("#".concat(modal)).modal({
+function handleKeyEvent(button, modal, saveField, confirmId){
+    $("#".concat(button)).click(function(e) {
+        e.preventDefault()
+        const jModal = $(this).modal({
             escapeClose: false,
             // showClose: false,
-            clickClose: false
-        })
+            clickClose: false,
+            beforeClose: function(e){
+                confirm(chosen, modal, confirmId);
+            },
+            focus: this,
+        });
+        $("#".concat(modal)).trigger('focus');
         $("#".concat(modal)).keydown(function(e) {
-        assignKeyBind(e,saveField);
+            $("#errorUp").text("");
+            assignKeyBind(e,saveField);
+        });
     });
-        var confirmId = '#confirmKeyUp';
-        const t = $(confirmId);
-        $(confirmId).click(function(e){
-            e.preventDefault();
-            console.log('checking if valid')
-            // chosenKeys[]
-        })
+    // $(document).on($.modal.CLOSE, function(e){
+    //     confirm(chosen, modal, confirmId);
+    //     // if (noConfirm) {
+    //     //     e.preventDefault();
+    // });
+    // $("#".concat(modal)).on('close', function(){
+    //     confirm(chosen, modal, confirmId);
+    // })
+    $("#".concat(confirmId)).click(function(e){
+        confirm(chosen, modal, confirmId);
+        // $("#".concat(modal)).modal().toggle();
+    })
 
-    });
 }
 
 function assignKeyBind(e, idSave) {
     chosen = e.code;
-
     $("#".concat(idSave)).text(chosen);
+}
+
+function confirm(key, modal, id) {
+    var error_message = "Duplication exists, Please choose another key"
+    if (id === "confirmKeyUp") {
+        if (key === chosenKeys.keyDown || key === chosenKeys.keyLeft || key === chosenKeys.keyRight) {
+            $("#errorUp").text(error_message);
+            noConfirm = true;
+        }
+        else {
+            chosenKeys.keyUp = key;
+            $.modal.close();
+            // $("#".concat(modal)).css("display", "none");
+        }
+    }
 }
 
 function outputNumber(val) {
